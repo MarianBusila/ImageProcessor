706c8ef887954aac4490d9164c6ade2669eee155
Marian Busila <marian.busila@gmail.com>
2019-04-27 21:37:11 -0400

    add cosmos db output binding and save metadata after image resize

--- ImageProcessor/ResizeImage.cs
+++ ImageProcessor/ResizeImage.cs
@@ -10,21 +10,32 @@ namespace ImageProcessor
 {
     public static class ResizeImage
     {
+
         [FunctionName("ResizeImage")]
         public static void Run(
-            [BlobTrigger("images/{name}", Connection = "AZURE_STORAGE_CONNECTION_STRING")]Stream imageStream,
-            [Blob("thumbnails/{name}", FileAccess.Write, Connection = "AZURE_STORAGE_CONNECTION_STRING")]Stream thumbnailImageStream,
-            string name, 
+            [BlobTrigger("images/{name}", Connection = "AZURE_STORAGE_CONNECTION_STRING")]
+            Stream imageStream,
+            [Blob("thumbnails/{name}", FileAccess.Write, Connection = "AZURE_STORAGE_CONNECTION_STRING")]
+            Stream thumbnailImageStream,
+            [CosmosDB("imagesdb", "images", Id = "id", ConnectionStringSetting = "COSMOS_DB", CreateIfNotExists = true)]
+            out dynamic document,
+            string name,
             ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {imageStream.Length} Bytes");
-            Image<Rgba32> originalImage = Image.Load(imageStream);            
+            Image<Rgba32> originalImage = Image.Load(imageStream);
             originalImage.Mutate(x => x.Resize(new ResizeOptions()
             {
                 Size = new Size(200, 200),
                 Mode = ResizeMode.Pad
             }));
             originalImage.SaveAsJpeg(thumbnailImageStream);
-        }
+            document = new 
+            {
+                id = name,
+                imgPath = "/images/" + name,
+                thumbnailPath = "/thumbnails/" + name
+            };
+        }    
     }
 }
--- README.md
+++ README.md
@@ -4,12 +4,15 @@ Serverless web app in Azure for image processing based on Microsoft [tutorial](h
 The tutorial covers:
 * Configure Azure Blob storage to host a static website and uploaded images.
 * Upload images to Azure Blob storage using Azure Functions.
+	* Azure Storage Explorer and Emulator can be used for local testing
 	* The website calls the azure function **GetUploadUrl** function to get a temporary URL where to upload the image
 	* The website uploads the image to **images** container from the storage account.
 * Resize images using Azure Functions.
 	* **ResizeImage** function is triggered when an image is uploaded and it resizes the image
 	* the resized image is placed in **thumbnails** container from the storage account
 * Store image metadata in Azure Cosmos DB.
+	* create a CosmosDB acount (CosmosDB emulator can also be used for local testing)
+	* create **imagesdb** database and **images** collection inside this database
 * Use Cognitive Services Vision API to auto-generate image captions.
 * Use Azure Active Directory to secure the web app by authenticating users.
 
